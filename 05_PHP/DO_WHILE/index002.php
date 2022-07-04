20c575b2537ad95874722c3029c308e8826741e8
Pedro <106389278+pfiguebar@users.noreply.github.com>
2022-07-04 12:45:17 +0200

    do while

---
+++ 05_PHP/DO_WHILE/index002.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>do while</title>
+
+</head>
+<body>
+
+<?php
+$contador = 1;
+do{
+    echo "<h$contador> hola mundo </h$contador><br>";
+    $contador++;
+}
+
+while($contador < 6)
+?>
+
+    
+</body>
+</html>
