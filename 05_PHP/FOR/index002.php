6abebab37fa267d0759b0d76a842f5dc484c4f86
Pedro <106389278+pfiguebar@users.noreply.github.com>
2022-07-04 12:18:09 +0200

    for

---
+++ 05_PHP/FOR/index002.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FOR</title>
+
+</head>
+<body>
+
+<?php
+
+for ($contador = 1; $contador < 6; $contador ++){
+echo "<h$contador> hola mundo </h$contador><br>";
+
+}
+?>
+
+    
+</body>
+</html>
