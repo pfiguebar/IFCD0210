f522fa49cbe54a0be77b876a11df870ac64f1c10
Pedro <106389278+pfiguebar@users.noreply.github.com>
2022-07-12 10:01:05 +0200

    Ejemplo SET e imprimir datos

--- 05_PHP/CLASES/clase002.php
+++ 05_PHP/CLASES/clase002.php
@@ -17,17 +17,10 @@ public $nombre_apellidos;
 public $dni; 
 public $sueldo; 
 
-public function Nombre_Apellidos_Imprimir(){
-    echo "$this->nombre_apellidos <br>";
-}
 
-public function Dni_Imprimir(){
-    echo "$this->dni <br>";
-}
-
-public function Sueldo_Imprimir(){
-    echo "$this->sueldo <br>";
-}
+public function SetNombre_Apellidos(){   $this->nombre_apellidos ; }
+public function SetDni(){    $this->dni ; }
+public function SetSueldo(){   $this->sueldo ; }
 
 }
 
@@ -36,19 +29,11 @@ $trabajador1->nombre_apellidos= "Juan Perez";
 $trabajador1->dni= "123456789D";
 $trabajador1->sueldo= "20000";
 
-$trabajador1->Nombre_Apellidos_Imprimir();
-$trabajador1->Dni_Imprimir();
-$trabajador1->Sueldo_Imprimir();
+echo $trabajador1->Nombre_Apellidos();
+echo $trabajador1->Dni();
+echo $trabajador1->Sueldo();
 
 
-$trabajador2 = new Trabajador;
-$trabajador2->nombre_apellidos= "Paco Garcia";
-$trabajador2->dni= "987654321D";
-$trabajador2->sueldo= "10000";
-
-$trabajador2->Nombre_Apellidos_Imprimir();
-$trabajador2->Dni_Imprimir();
-$trabajador2->Sueldo_Imprimir();
 
 ?>
 
