f190620a69bb89a47577740d2372bfb46e30e3af
Pedro <106389278+pfiguebar@users.noreply.github.com>
2022-07-13 10:15:39 +0200

    Clases Heredadas - Polimorfismos

---
+++ 05_PHP/CLASES/clases012.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>get y set</title>
+</head>
+<body>
+    
+<?php 
+class Articulo{
+    public $nombre;
+    public $precio;
+    
+
+    public function __construct($nombre, $precio)
+    {
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+    }
+
+    // DAME INFO
+    public function pintaArticulo(){
+        echo "
+        <div>
+        <h3>Nombre: $this->nombre </h3>
+        <h4>Precio: $this->precio </h4>
+        </div>
+        ";
+        
+    }
+
+}
+
+        class Articulo_rebajado extends Articulo{
+            public $precio_rebajado;
+            // la herencia constructor es especial. Heredo estructura pero no valores variables 
+            // Hay que indicar parent:: __construct($variable1, $variable2);
+            // Tambien hay que añadir variable1 y variable2 a funcion constructor
+            public function __construct($nombre, $precio, $precio_rebajado)
+            {
+                parent:: __construct($nombre, $precio);
+                $this->precio_rebajado = $precio_rebajado;
+            }
+
+            // DAME INFO  (metodo2 = polimorfismo)   
+            public function pintaArticulo(){
+                parent::pintaArticulo();
+                echo "Precio rebajado: $this->precio_rebajado";
+            }
+
+
+        }
+
+
+
+
+
+// Las 2 variables __construct clase PADRE
+$articulo1 = new Articulo("Bicicleta", "1500"); 
+
+// Las 3 variables __construct clase HIJO
+$articulo_rebajado1 = new Articulo_Rebajado ("Bicicleta", "1500", "1200");
+
+// LLamar funcion imprimir
+$articulo_rebajado1->pintaArticulo();
+?>
+</body>
+</html>
