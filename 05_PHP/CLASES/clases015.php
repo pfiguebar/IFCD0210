146bbdf75f3b676bc30031a604882dfcdf915dba
Pedro <106389278+pfiguebar@users.noreply.github.com>
2022-07-20 13:21:36 +0200

    Clases Magicas (toString)

--- 05_PHP/CLASES/clases015.php
+++ 05_PHP/CLASES/clases015.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clases magicas</title>
+</head>
+<body>
+    
+
 <?php
 class Animal
 {
@@ -11,3 +22,8 @@ $raza1 = new Animal('Perro');
 echo $raza1;
 
 ?>
+
+ 
+
+</body>
+</html>
