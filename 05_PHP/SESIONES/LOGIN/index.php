37b3965bb6578ea7e15b48b05b56fe52c9e9d624
Pedro <106389278+pfiguebar@users.noreply.github.com>
2022-07-23 11:47:55 +0200

    index login

--- 05_PHP/SESIONES/LOGIN/index.php
+++ 05_PHP/SESIONES/LOGIN/index.php
@@ -12,7 +12,7 @@ si no existe variable ..... redireccionar a menu login para meter datos
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LOGIN</title>
 </head>
 <body>
 
