e25f0dc97e5e47df4a8b9ea37b9c9604fe36ca23
Pedro <106389278+pfiguebar@users.noreply.github.com>
2022-07-23 11:45:45 +0200

    INDEX_LOGIN

--- 05_PHP/SESIONES/LOGIN/login.php
+++ 05_PHP/SESIONES/LOGIN/login.php
@@ -16,57 +16,9 @@ si no existe variable ..... redireccionar a menu login para meter datos
 </head>
 <body>
 
-<nav>
-    <ul>
-        <li>LOGIN <br>
-        
-        <!-- 
-            isset = si existe (variable session usuario)... haz esto: 
-                a) imprime bienvenido usuario 
-                b) imprime boton Logout  
-        --> 
-        <?php if(isset($_SESSION["usuario"])) 
-        {
-            echo "Bienvenido: ";
-            echo $_SESSION["usuario"];
-            
-            echo "<br>";
-            
-            ?>
+<?php include 'nav.php' ?>    
 
-            <!-- link logout lo que hace es cargar php destruir sesion -->
-            <a href="
-
-                <?php 
-                session_destroy();
-                ?>
-                       
-            ">LOGOUT</a>
-
-
-            <?php
-
-        }
-        ?>
-
-        <!-- fin isset -->
-        </li>
-
-        
-        
-
-
-    </ul>
-</nav>    
-
-<form action="" method="POST" >
-    <input type="text" id="usuario" name="usuario">
-    <input type="text" id="password" name="password">
-    
-    <input type="submit" id="enviar" name="enviar">
-</form>
-
-<?php incluide 'session.php' ?>
+<?php include 'session.php' ?>
     
 </body>
 </html>
